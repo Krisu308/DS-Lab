e27b94084a90984fdcf030b4ec539348c9350776
Krisu308 <krishvi01126@gmail.com>
2026-01-20 20:26:42 +0530

    Update pro1.c

--- DS/lab 3/pro1.c
+++ DS/lab 3/pro1.c
@@ -4,7 +4,7 @@ int main() {
     int A[2][2], B[2][2], C[2][2];
     int i, j;
 
-    // Read first matrix
+    
     printf("Enter elements of Matrix A:\n");
     for(i = 0; i < 2; i++) {
         for(j = 0; j < 2; j++) {
@@ -12,7 +12,7 @@ int main() {
         }
     }
 
-    // Read second matrix
+    
     printf("Enter elements of Matrix B:\n");
     for(i = 0; i < 2; i++) {
         for(j = 0; j < 2; j++) {
@@ -20,14 +20,14 @@ int main() {
         }
     }
 
-    // Add matrices
+    
     for(i = 0; i < 2; i++) {
         for(j = 0; j < 2; j++) {
             C[i][j] = A[i][j] + B[i][j];
         }
     }
 
-    // Print result matrix
+    
     printf("Addition of two matrices (Matrix C):\n");
     for(i = 0; i < 2; i++) {
         for(j = 0; j < 2; j++) {
@@ -38,3 +38,4 @@ int main() {
 
     return 0;
 }
+
